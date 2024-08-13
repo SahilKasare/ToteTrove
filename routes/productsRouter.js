d9b5d2a746fa323d14251e6b28af735958208594
SahilKasare <cdsahilkasare@gmail.com>
2024-08-13 16:58:38 +0530

    Error correction

--- routes/productsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function(req, res){
-    res.send("Hello into the Products.")
-});
-
-module.exports = router;
